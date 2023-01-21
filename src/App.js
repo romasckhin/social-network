dbd4ba7f00f6bc6be7a68047eb5e197a04e70b77
Roman <cikipuki10@gmail.com>
2023-01-21 21:58:27 +0200

    react router dom v6

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs'
-
+import {Routes, Route} from 'react-router-dom'
 
 const App = () => {
 
@@ -14,8 +14,10 @@ const App = () => {
       <Navbar/>
 
       <div className='app-wrapper-content'>
-        <Dialogs/>
-        <Profile/>
+        <Routes>
+          <Route path='/dialogs' element={<Dialogs/>} />
+          <Route path='/profile' element={<Profile/>} />
+        </Routes>
       </div>
       
     </div>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,14 +1,29 @@
+import { NavLink } from 'react-router-dom';
 import cls from './Navbar.module.css'
+
+const setActive = ({isActive}) => isActive ? cls.active : ''
 
 const Navbar = () => {
     return (
         <nav className={cls.nav}>
-            <div className={cls.item}>Profile</div>
-            <div className={cls.item}>Message</div>
-            <div className={cls.item}>News</div>
-            <div className={cls.item}>Misic</div>
+            <div>
+                <NavLink to="/profile"  className={ setActive } >Profile</NavLink>
+            </div>
+            <div>
+                 <NavLink to="/dialogs"  className={ setActive} >Message</NavLink> 
+            </div>
+            {/* <div>
+                 <NavLink to="/" className={ ({isActive}) => isActive ? cls.active : ''}>News</NavLink> 
+            </div>
+            <div>
+                 <NavLink to="/" className={ ({isActive}) => isActive ? cls.active : ''}>Misic</NavLink> 
+            </div>
+            <div>
+                 <NavLink to="/" className={ ({isActive}) => isActive ? cls.active : ''}>Settings</NavLink> 
+            </div> */}
         </nav>
     );
 };
 
 export default Navbar;
+
