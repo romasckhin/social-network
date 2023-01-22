78ef2213a7faea58440f460d0d841ecf2771a46e
Roman <cikipuki10@gmail.com>
2023-01-22 20:21:09 +0200

    update Dialogs

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -2,8 +2,32 @@ import cls from './Dialogs.module.css'
 
 const Dialogs = () => {
     return (
-        <div className={cls.content}>
-            Dialogs
+        <div className={cls.dialogs}>
+
+           <div className={cls.dialogsItems}>
+                <div className={cls.dialog}>
+                    Roman
+                </div>
+                <div className={cls.dialog}>
+                    Alina
+                </div>
+                <div className={cls.dialog}>
+                    Sabina
+                </div>
+                <div className={cls.dialog}>
+                    Arsen
+                </div>
+                <div className={cls.dialog}>
+                    Mandarina
+                </div>
+           </div>
+
+            <div className={cls.messages}>
+                <div className={cls.message}>Hi</div>
+                <div className={cls.message}>House</div>
+                <div className={cls.message}>Apartament</div>
+            </div>
+
         </div>
     );
 };
